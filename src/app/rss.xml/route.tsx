42a6dca29e9a9885c5d2f76e54012e300bf73666
Ben Gubler <nebrelbug@gmail.com>
2024-06-25 00:55:08 -0600

    feat: render MDX to HTML in feed

--- src/app/rss.xml/route.tsx
+++ src/app/rss.xml/route.tsx
@@ -1,9 +1,12 @@
 import { Feed } from "feed";
 
 import { baseUrl } from "@/lib/config";
-import { getPosts } from "@/lib/get-posts";
 
-const createFeed = () => {
+import { MDXContent } from "@content-collections/mdx/react";
+import { allPosts } from "content-collections";
+import { NextRequest, NextResponse } from "next/server";
+
+const createFeed = (renderToString: Function) => {
   const feed = new Feed({
     title: "bengubler.com",
     description: "Ben Gubler's personal website",
@@ -17,34 +20,44 @@ const createFeed = () => {
     },
   });
 
-  const allPosts = getPosts({
-    localOnly: true,
-  });
+  const posts = allPosts
+    .sort((a, b) => {
+      return new Date(b.date).getTime() - new Date(a.date).getTime();
+    })
+    .map((post) => ({
+      ...post,
+      url: `/posts/${post._meta.path}`,
+    }));
 
-  allPosts.forEach((post) => {
+  for (const post of posts) {
+    const html = renderToString(<MDXContent code={post.mdx} />);
+
     feed.addItem({
       title: post.title,
       id: `${baseUrl}${post.url}`,
       link: `${baseUrl}${post.url}?utm_campaign=feed&utm_source=rss2`,
       description: post.description,
-      content: post.content,
+      content: html,
       date: post.date,
       category: post.tags
         ? post.tags.map((name) => ({ name: name }))
         : undefined,
       // image: createImageUrl(post.image.url, 256, 256, true),
     });
-  });
+  }
 
   return feed.rss2();
 };
 
-export const GET = async () => {
-  const feed = await createFeed();
-  return new Response(feed, {
+export async function GET(req: NextRequest): Promise<NextResponse> {
+  const ReactDOMServer = (await import("react-dom/server")).default;
+
+  const feed = createFeed(ReactDOMServer.renderToString);
+
+  return new NextResponse(feed, {
     status: 200,
     headers: {
       "Content-Type": "application/xml",
     },
   });
-};
+}
