6d8b0a0087ba2419057e59f4b6fe3b33417cb1cf
Ben Gubler <nebrelbug@gmail.com>
2025-06-14 22:22:04 -0700

    feat: update *.md handling to use rewrites in tutorial/personal blog

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import { withContentCollections } from "@content-collections/next";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/posts/:slug.md",
+        destination: "/api/posts/:slug/raw",
+      },
+    ];
+  },
 };
 
 // withContentCollections must be the outermost plugin
